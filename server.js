8323acacd44bc7f905717ee2d9f461cc665591a2
Aastha-Desai <aasthadesai131@gmail.com>
2025-03-01 23:05:11 -0500

    commiting all

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-const port = 3000; // You can change the port if needed
+const port = 3000;
 
-// Serve static files (your HTML, CSS, JS)
-app.use(express.static(path.join(__dirname, '')));
+// Serve static files from the 'Home_Page' folder
+app.use(express.static(path.join(__dirname, 'Home_Page')));
 
-// When accessing the root, serve the index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'Home_Page', 'index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
